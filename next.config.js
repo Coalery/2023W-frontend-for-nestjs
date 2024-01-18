66fafd66bb4eb16d8dec965377dec715ace0a3d8
Coalery <doralife12@gmail.com>
2024-01-18 21:57:46 +0900

    chore: asset prefix 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export'
+  output: 'export',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/2023W-frontend-for-nestjs'
 }
 
 module.exports = nextConfig
